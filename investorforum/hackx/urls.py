e0ec2511524aa68c70bc9fd980f05c43b7a6ca9f
exodus31 <sarthak.nandre31@gmail.com>
2022-08-25 11:48:48 +0530

    Basic Setup

--- investorforum/hackx/urls.py
+++ investorforum/hackx/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="index"),
-
+    path('signup', views.signup, name="signup"),
+    path('signup_ent', views.signup_ent, name="signup_ent"),
+    path('signup_inv', views.signup_inv, name="signup_inv"),
 ]
--- investorforum/hackx/views.py
+++ investorforum/hackx/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    return render(request, 'signup1.html')
+
+def signup_ent(request):
+    return render(request, 'signup_ent.html')
+
+def signup_inv(request):
+    return render(request, 'signup_inv.html')
